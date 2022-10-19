28fd2d3113b4440003d2446f1ead7f012f82ae76
thafer1 <112597461+ZedZappie@users.noreply.github.com>
2022-10-18 23:33:09 -0400

    Added functionality to save buttons

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,10 @@
 // Displays current day
 var currentDay = moment().format("dddd, MMMM Do")
 $("#currentDay").text(currentDay);
+
+// Functionality for save buttons
+$(".saveBtn").on("click", function() {
+    userInput = $(this).siblings(".form-control").val();
+    selectedHour = $(this).siblings(".input-group-text").text();
+    localStorage.setItem(selectedHour, userInput);
+  });
